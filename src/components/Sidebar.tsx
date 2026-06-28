5a4c42bb4720e7ab0ffaf8bb5832ec9c46de999d
ilhamsptr1 <ilham09saputraaa@gmail.com>
2026-06-29 01:00:37 +0700

    fix: use FiInstagram from react-icons/fi to fix missing Instagram icon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Instagram, Mail } from "lucide-react";
-import { FaGithub, FaTiktok } from "react-icons/fa";
+import { FaGithub, FaTiktok, FaEnvelope } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 import { useThemeSound } from "@/context/ThemeSoundContext";
 import MagneticWrapper from "./MagneticWrapper";
 
 const socialLinks = [
   { icon: <FaGithub className="w-6 h-6" />, href: "https://github.com/ilhamsptr1", color: "blue" },
-  { icon: <Instagram className="w-6 h-6" />, href: "https://www.instagram.com/ilhammsptra_/", color: "green" },
+  { icon: <FiInstagram className="w-6 h-6" />, href: "https://www.instagram.com/ilhammsptra_/", color: "green" },
   { icon: <FaTiktok className="w-6 h-6" />, href: "https://www.tiktok.com/@ninetofive925", color: "blue" },
-  { icon: <Mail className="w-6 h-6" />, href: "mailto:ilham0909saputraaa@gmail.com", color: "green" },
+  { icon: <FaEnvelope className="w-6 h-6" />, href: "mailto:ilham0909saputraaa@gmail.com", color: "green" },
 ];
 
 export default function Sidebar() {
